f1b144f80774ec8b9748bd44be2b7fa609a7b665
Miguel Negrão <miguel.negrao@friendlyvirus.org>
2013-01-17 12:33:48 +0000

    update

--- src/org/friendlyvirus/tabletSliders/TabletSlidersActivity.java
+++ src/org/friendlyvirus/tabletSliders/TabletSlidersActivity.java
@@ -2,18 +2,20 @@ package org.friendlyvirus.tabletSliders;
 
 import android.os.*;
 import android.util.Log;
-import android.view.MotionEvent;
 import controlP5.*;
 import processing.core.*;
 // Import oscP5 and netP5 libraries
 import oscP5.*;
 import netP5.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class TabletSlidersActivity extends PApplet {
 
     //String ip = "192.168.1.102";
-    String ip = "192.168.1.1";
+    String ip = "192.168.0.14";
     NetAddress scClient = new NetAddress( ip, 57120);
 
     //create one worker thread
@@ -25,6 +27,13 @@ public class TabletSlidersActivity extends PApplet {
     int numRows = 2;
     float horizontalSpacingRatio = 0.4f;
     float verticalSpacingRatio = 0.2f;
+
+    Map<Slider,int[]> sliderMap = new HashMap<Slider, int[]>();
+
+    int tabHeight = 30;
+
+    String[] pages = { "Page 2", "Page 3" };
+    String[] allpages = { "default", "Page 2", "Page 3" };
 
     class OSCThread extends Thread {
         public Handler mHandler;
@@ -51,32 +60,78 @@ public class TabletSlidersActivity extends PApplet {
 
         cp5 = new ControlP5(this);
 
+        int tabWidth = (int) ((float) width) / allpages.length;
+
+        for( String label : pages ) {
+            cp5.addTab(label)
+                    .setColorBackground(color(0, 160, 100))
+                    .setColorLabel(color(255))
+                    .setColorActive(color(255, 128, 0))
+                    .setWidth(tabWidth)
+                    .setHeight(tabHeight)
+            ;
+        }
+
+        cp5.getTab("default")
+                .setLabel("Page 1")
+                .setWidth(tabWidth)
+                .setHeight(tabHeight)
+        ;
+
         int count = 1;
 
-        int horizontalSize = (int) (((float) width) * (1 - horizontalSpacingRatio) / numSlidersPerRow);
-        int verticalSize = (int) (((float) height) * (1 - verticalSpacingRatio) / numRows);
-        float horizontalSpacing = width * horizontalSpacingRatio / (numSlidersPerRow + 1);
-        float verticalSpacing = height * verticalSpacingRatio / (numRows + 1);
 
-        for (int i = 0; i < numRows; i++)
-            for (int j = 0; j < numSlidersPerRow; j++) {
-                cp5.addSlider("" + count)
-                        .setPosition(
-                                horizontalSpacing + (j * (horizontalSpacing + horizontalSize)),
-                                verticalSpacing + (i * (verticalSpacing + verticalSize))
-                        )
-                        .setSize(horizontalSize, verticalSize)
-                        .setRange(0.0f, 1.0f)
-                        .setId(count-1);
-                count++;
-            }
+
+        int [][] sliderSetup = { {3,12}, {2, 12}, {2,12} };
+
+        for( int k = 0; k < allpages.length; k++ ) {
+
+            int numRows = sliderSetup[k][0];
+            int numSlidersPerRow = sliderSetup[k][1];
+
+            int horizontalSize = (int) (((float) width) * (1 - horizontalSpacingRatio) / numSlidersPerRow);
+            int verticalSize = (int) (((float) height) * (1 - verticalSpacingRatio) / numRows);
+            float horizontalSpacing = width * horizontalSpacingRatio / (numSlidersPerRow + 1);
+            float verticalSpacing = height * verticalSpacingRatio / (numRows + 1);
+
+            for (int i = 0; i < numRows; i++)
+                for (int j = 0; j < numSlidersPerRow; j++) {
+                    Slider sl = cp5.addSlider(k+" - "+i+" - "+j)
+                            .setPosition(
+                                    horizontalSpacing + (j * (horizontalSpacing + horizontalSize)),
+                                    verticalSpacing + (i * (verticalSpacing + verticalSize))
+                            )
+                            .setSize(horizontalSize, verticalSize)
+                            .setRange(0.0f, 1.0f)
+                            .setId(count-1)
+                            .moveTo( allpages[k] );
+                    count++;
+                    int[] x = { k, i, j };
+                    sliderMap.put( sl, x );
+                }
+        }
+
+        //for debugging
+        /*
+        Textarea myTextarea = cp5.addTextarea("txt")
+                .setPosition(100, 600)
+                .setSize(800, 100)
+                .setFont(createFont("", 10))
+                .setLineHeight(14)
+                .setColor(color(200))
+                .setColorBackground(color(0, 100))
+                .setColorForeground(color(255, 100));
+        ;
+
+        Println console = cp5.addConsole(myTextarea);
+        console.play();
+        */
 
     }
 
 
     public void draw() {
         background(0);
-
     }
 
 
@@ -86,11 +141,16 @@ public class TabletSlidersActivity extends PApplet {
            which of the controllers has been changed.
         */
 
-        println("got a control event from controller with id "+theEvent.getController().getId());
+
         float value = theEvent.getValue();
-        int id = theEvent.getController().getId();
+        Slider sl = (Slider) theEvent.getController();
+        int[] slCode = sliderMap.get(sl);
+        println("got a control event from controller with code "+slCode[0]+" "+slCode[1]+" "+slCode[2]+" label: " +
+                ""+theEvent.getController().getLabel());
         OscMessage oscMsg = new OscMessage("/slider");
-        oscMsg.add(id);
+        for( int i : slCode ) {
+            oscMsg.add(i);
+        }
         oscMsg.add(value);
 
         Message msg = Message.obtain();
